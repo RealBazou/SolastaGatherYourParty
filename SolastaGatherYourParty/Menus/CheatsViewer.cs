be50f74eed4744d8859e4b1931d003f97d116356
ThyWoof <pauloesquilo@gmail.com>
2021-06-13 15:52:45 -0700

    refactor mod UI

---
+++ SolastaGatherYourParty/Menus/CheatsViewer.cs
@@ -0,0 +1,27 @@
+﻿using UnityModManagerNet;
+using ModKit;
+using static SolastaGatherYourParty.Main;
+
+namespace SolastaGatherYourParty.Menus
+{
+    public class CheatsViewer : IMenuSelectablePage
+    {
+        public string Name => "Cheats";
+
+        public int Priority => 2;
+
+        public void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            if (Mod == null || !Mod.Enabled)
+                return;
+
+            UI.Toggle("Invincible Party", ref settings.InvincibleParty, 0, UI.AutoWidth());
+            UI.Toggle("Idle Enemies", ref settings.IdleEnemies, 0, UI.AutoWidth());
+            UI.Toggle("Fog of War", ref settings.FogOfWar, 0, UI.AutoWidth());
+
+            Cheats.SetPartyInvicible(settings.InvincibleParty);
+            Cheats.SetMonstersIdle(settings.IdleEnemies);
+            Cheats.SetFogOfWar(settings.FogOfWar);
+        }
+    }
+}
