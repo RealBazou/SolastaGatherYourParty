d73e06051ae33c5526c1f61b11d9d646d79c53d6
ThyWoof <pauloesquilo@gmail.com>
2021-06-13 10:36:02 -0700

    initial commit by SolastaMod

---
+++ SolastaGatherYourParty/Patches/GameManagerPatcher.cs
@@ -0,0 +1,16 @@
+using HarmonyLib;
+
+namespace SolastaGatherYourParty.Patches
+{
+    class GameManagerPatcher
+    {
+        [HarmonyPatch(typeof(GameManager), "BindPostDatabase")]
+        internal static class GameManager_BindPostDatabase_Patch
+        {
+            internal static void Postfix()
+            {
+                Main.ModEntryPoint();
+            }
+        }
+    }
+}
