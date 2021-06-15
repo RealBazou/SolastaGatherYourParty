4cfdad799ddf668e3e71fb0f9e60b037d799e9a6
ThyWoof <pauloesquilo@gmail.com>
2021-06-14 20:01:40 -0700

    nothing important

---
+++ SolastaGatherYourParty/Patches/AiLocationManagerPatcher.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+
+namespace SolastaGatherYourParty.Patches
+{
+    class AiLocationManagerPatcher
+    {
+        [HarmonyPatch(typeof(AiLocationManager), "CharacterCreated")]
+        internal static class AiLocationManager_CharacterCreated_Patch
+        {
+            internal static void Prefix(AiLocationManager __instance, GameLocationCharacter character)
+            {
+                var controllers = ServiceRepository.GetService<IPlayerControllerService>();
+
+                if (character.ControllerId == 8)
+                {
+                   
+                }
+            }
+        
+            internal static void Postfix(AiLocationManager __instance, GameLocationCharacter character)
+            {
+                var controllers = ServiceRepository.GetService<IPlayerControllerService>();
+
+                if (character.ControllerId == 8)
+                {
+                    
+                }
+            }
+        }
+    }
+}
